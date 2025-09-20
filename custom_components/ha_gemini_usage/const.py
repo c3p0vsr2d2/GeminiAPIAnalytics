83b5d29eeb2f99fe6352053e0c2097e3c7ea85a5
c3p0vsr2d2 <90661423+c3p0vsr2d2@users.noreply.github.com>
2025-09-20 14:56:16 -0700

    Create const.py

---
+++ custom_components/ha_gemini_usage/const.py
@@ -0,0 +1,15 @@
+"""Constants for the Google Gemini Usage integration."""
+
+DOMAIN = "gemini_usage"
+CONF_API_KEY = "api_key"
+COORDINATOR = "coordinator"
+UNDO_UPDATE_LISTENER = "undo_update_listener"
+
+# Attributes
+ATTR_MODEL = "model"
+ATTR_INPUT_TOKENS = "input_tokens"
+ATTR_OUTPUT_TOKENS = "output_tokens"
+ATTR_TOTAL_TOKENS = "total_tokens"
+
+# Sensor Names
+SENSOR_CALLS_NAME = "Total API Calls"
